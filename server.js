4447712f3d99acdfe9006a5996ecb7cda3bd6e34
Unnat1654 <125393204+unnat1654@users.noreply.github.com>
2023-08-31 15:42:28 +0530

    deploying

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "./dist")));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
@@ -35,7 +35,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
